66f9c069a066b3826a976779e801fc94090ae90f
caiobrandini <66636442+caiobrandini@users.noreply.github.com>
2023-07-20 00:36:29 -0300

    feat: little changes

--- core/monitoring/urls.py
+++ core/monitoring/urls.py
@@ -1,6 +1,7 @@
-from monitoring.views import CreateStockListener
 from django.urls import path
+from monitoring.views import CreateStockListener, DeleteStockListener
 
 urlpatterns = [
 	path("create/", CreateStockListener.as_view(), name="create_stock_listener"),
+	path("delete/", DeleteStockListener.as_view(), name="delete_stock_listener"),
 ]
